dea52f90d8d43968e864202713510505dbad0fa2
N M Nayana <nmnayana62@gmail.com>
2023-09-21 13:02:41 +0530

    feat: responsive

--- src/pages/MovieDetails/MovieDetails.js
+++ src/pages/MovieDetails/MovieDetails.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "../../axios";
 import { useSelector } from "react-redux";
 import Header from "../../components/Header";
+import { truncateText } from "../../components/TruncateText";
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
@@ -40,16 +41,16 @@ const MovieDetails = () => {
     <>
       <Header />
       {loading ? (
-        <div className="w-full h-[calc(100vh-5rem)] flex justify-center items-center">
+        <div className="w-full h-[calc(100vh-5rem)] min-h-full flex justify-center items-center">
           <h2 className="w-full h-full flex justify-center items-center text-2xl font-semibold">
             Loading...
           </h2>
         </div>
       ) : (
-        <div className=" text-white h-[calc(100vh-5rem)] bg-blue-400">
+        <div className=" text-white h-[calc(100vh-5rem)] min-h-full bg-blue-400">
           {error === "" ? (
-            <div className="flex flex-col sm:flex-row  gap-10  h-full items-center justify-center">
-              <div className="w-[330px] h-[480px] rounded-xl">
+            <div className="flex flex-col sm:flex-row gap-3 sm:gap-10  h-full items-center justify-center py-10 sm:py-2">
+              <div className=" w-[250px] sm:w-[330px] h-[350px] sm:h-[480px] rounded-xl">
                 <img
                   src={`${base_url}${movie.poster_path}`}
                   alt={movie.name}
@@ -58,11 +59,13 @@ const MovieDetails = () => {
                 />
               </div>
               <div className=" w-80 sm:w-[900px]">
-                <div className="flex flex-col sm:flex-row gap-2 items-start sm:items-end">
-                  <h2 className=" text-4xl font-bold">
+                <div className="inline">
+                  <h2 className="inline text-4xl font-bold w-[800px]">
                     {movie?.title || movie?.name}
                   </h2>
-                  <p className="text-3xl"> ( {movie.release_date} )</p>
+                  <p className="inline text-3xl">
+                    ({movie.release_date.toString().slice(0, 4)})
+                  </p>
                 </div>
                 <p className="mb-2 font-semibold text-lg">
                   {movie.vote_average} / 10
